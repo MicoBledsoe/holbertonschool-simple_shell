91555108d733375726749121868b0de85098abb8
MicoBledsoe <MicoMods@gmail.com>
2022-11-28 16:04:50 -0800

    Adding .h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+#include <limits.h>
+
+#endif
