f41435280c3a66ad6079c790341f92cfac579b48
MicoBledsoe <MicoMods@gmail.com>
2022-12-05 11:12:36 -0800

    Adding Environment

---
+++ _enviornment.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "shell.h"
+
+/**
+ * _environment - gets environment for path
+ * @name: const char
+ * Return: NULL
+ */
+
+char *_getenv(char *name)
+{
+	int i;
+	char *pathvalue;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strncmp(environ[i], name, 4) == 0)
+		{
+			pathvalue = _strdup(&environ[i][5]);
+			return (pathvalue);
+		}
+	}
+	return (NULL);
+}
