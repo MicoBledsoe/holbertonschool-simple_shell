46ae63ead0c7c7c977b4d628bf341e263fc308f0
MicoBledsoe <MicoMods@gmail.com>
2022-12-12 11:56:05 -0800

    update

--- file.c
+++ file.c
@@ -1,45 +1,62 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "shell.h"
 
-int main(void)
-{
-    char command[1024];
+int main(int argc, char **argv) {
+  // Set up some variables
+  size_t len = 1024;
+  char *buffer = NULL, *prompt, *command, *token;
+  pid_t cpid;
+  int status, exstat = 0;
 
-    // display prompt and wait for user input
-    while (1) {
-        printf("simple_shell> ");
-        scanf("%s", command);
-
-        // fork a new process
-        int pid = fork();
-
-        // check for errors in fork
-        if (pid == -1) {
-            perror("fork");
-            exit(1);
-        }
-
-        // child process
-        else if (pid == 0) {
-            // execute the command
-            int ret = execlp(command, command, NULL);
-
-            // check for errors in execution
-            if (ret == -1) {
-                perror("execlp");
-                exit(1);
-            }
-        }
-
-        // parent process
-        else {
-            // wait for child process to finish
-            wait(NULL);
-        }
+  // Loop forever (until an EOF or error is encountered)
+  while (1) {
+    // Print the prompt, if we are in interactive mode
+    if (isatty(STDIN_FILENO) == 1) {
+      prompt = "($) ";
+      puts(prompt);
     }
 
-    return 0;
+    // Read a line from the input
+    if (getline(&buffer, &len, stdin) == -1) {
+      break;
+    }
+
+    // Tokenize the input into separate commands
+    token = strtok(buffer, "\n");
+    command = strtok(buffer, " ");
+
+    // If the command is "env", print the environment
+    if (strcmp(buffer, "env") == 0) {
+      for (char **env = environ; *env; env++) {
+        printf("%s\n", *env);
+      }
+      continue;
+    }
+
+    // If the command is not found, print an error message
+    if (access(command, F_OK) == -1) {
+      fprintf(stderr, "%s: command not found\n", command);
+      continue;
+    }
+
+    // Create a child process to execute the command
+cpid = fork();
+
+// If the fork failed, print an error message and exit
+if (cpid == -1) {
+  perror("fork");
+  exit(1);
 }
 
+// If we are in the child process, execute the command
+if (cpid == 0) {
+  if (execvp(command, argv) == -1) {
+    perror("execvp");
+    exit(1);
+  }
+}
