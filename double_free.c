ea6e0023b08f2da81ff895bdd8a944381f4c7516
LW068 <105394574+LW068@users.noreply.github.com>
2022-12-13 14:16:48 -0600

    Create double_free.c

--- double_free.c
+++ double_free.c
@@ -3,6 +3,7 @@
 /**
  * double_free - free double pointer
  * @array: some double pointer array
+ * Detailed Description: This code defines the double_free() function, which frees the memory allocated for a double pointer array. The function takes a double pointer array as an argument and iterates over each element of the array, freeing the memory associated with each element. This is useful for freeing the memory allocated for an array of strings, such as the array variable used in the main() function of the shell. The function does not return a value. */
  * Return: void
  */
 void double_free(char **array)
