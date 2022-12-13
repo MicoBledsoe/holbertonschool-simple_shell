ad08a38d6e35c19bfe4aaa7b0de9c72cff2f773e
MicoBledsoe <MicoMods@gmail.com>
2022-12-12 23:25:14 -0800

    asdba

--- builtins.c
+++ builtins.c
@@ -2,26 +2,29 @@
 #include <stdio.h>
 
 /**
- * builtin_exit - function that exits the shell
+ * builtfunc - function that implements the exit builtin
  * @buf: buffer
  * @args: the parsed arguments
  * @env: the environment
  * @exstat: exit status
  * Return: void
  */
-void builtin_exit(char **buf, char **args, char **env, int *exstat)
+void builtfunc(char **buf, char **args, char **env, int *exstat)
 {
-    /* Free memory used by dynamically allocated variables */
-    free(buf);
-    free(args);
-    free(env);
+        /* Check if the first argument is "exit" */
+        if (_strcmp(args[0], "exit") == 0)
+        {
+                /* Free memory used by dynamically allocated variables */
+                free(buf);
+                free(args);
+                free(env);
 
-    /* Close any open file streams */
-    fclose(stdin);
-    fclose(stdout);
-    fclose(stderr);
+                /* Close any open file streams */
+                fclose(stdin);
+                fclose(stdout);
+                fclose(stderr);
 
-    /* Exit the shell with the specified exit status */
-    exit(*exstat);
+                /* Exit the shell with the specified exit status */
+                exit(*exstat);
+        }
 }
-
