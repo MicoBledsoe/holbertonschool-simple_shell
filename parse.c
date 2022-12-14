f0353b066651b630db1d80dfab06b0c11d96fb48
LW068 <ricardocoronaberron@gmail.com>
2022-12-14 12:55:16 -0800

    [added] revised version of parse.c file (test).

--- parse.c
+++ parse.c
@@ -21,33 +21,31 @@ int print_string(char *str)
 
 int parse_line(char *str)
 {
-	int bufsize = BUFFER_SIZE, count = 0;
-/* allocating buffer size for amount of tokens */
-	char *tokens[1024];
-	char *token;
+    int count = 0, bufsize = BUFFER_SIZE;
+    char *tokens[1024], *token, *temp;
+    const char *delim = " ";
 
-/* declaring the space as a delimiter */
-	const char *delim = " ";
-/* declaring a pointer to the first token */
-	token = strtok(str, delim);
+    temp = strdup(str);
+    if (!temp)
+        return (-1);
 
-	if (!token)
-		return (-1);
-/* walk through other tokens */
-	while (token != NULL)
-	{
-/* token is equal to the count of tokens */
-		tokens[count++] = token;
-/* if buffer size is reached, function has failed */
-		if (count == bufsize)
-			return (-1);
-/* token is equal to the last token which is NULL */
-		token = strtok(NULL, delim);
-	}
-/* return number of tokens */
-/*	printf("%s\n", tokens[count - 1]);*/
-	print_string(tokens[count - 1]);
-	print_string("\n");
-	tokens[count] = NULL;
-	return (count);
+    token = strtok(temp, delim);
+    if (!token)
+        return (-1);
+
+    while (token != NULL)
+    {
+        tokens[count++] = token;
+        if (count == bufsize)
+            return (-1);
+        token = strtok(NULL, delim);
+    }
+
+    print_string(tokens[count - 1]);
+    print_string("\n");
+    tokens[count] = NULL;
+
+    free(temp);
+
+    return (count);
 }
