e65b730a9c0cb00fbeda3c65c566154c69acceae
LW068 <105394574+LW068@users.noreply.github.com>
2022-12-14 11:48:48 -0600

    reverted parse.c file

--- parse.c
+++ parse.c
@@ -21,37 +21,33 @@ int print_string(char *str)
 
 int parse_line(char *str)
 {
-        int bufsize = BUFFER_SIZE, count = 0;
-        char *tokens[1024];
-        char *token;
+	int bufsize = BUFFER_SIZE, count = 0;
+/* allocating buffer size for amount of tokens */
+	char *tokens[1024];
+	char *token;
 
-        const char *delim = " ";
-        token = strtok(str, delim);
+/* declaring the space as a delimiter */
+	const char *delim = " ";
+/* declaring a pointer to the first token */
+	token = strtok(str, delim);
 
-        if (!token)
-                return (-1);
-
-        /* Limit the number of tokens to 1024 */
-        while (token != NULL && count < 1024)
-        {
-                tokens[count++] = token;
-                token = strtok(NULL, delim);
-        }
-
-        /* Check if the array was completely filled */
-        if (count == 1024)
-        {
-                fprintf(stderr, "Too many tokens in input string!\n");
-                return (-1);
-        }
-
-        print_string(tokens[count - 1]);
-        print_string("\n");
-
-        /* Set the last element of the array to NULL */
-        tokens[count] = NULL;
-
-        /* Free the memory allocated to the tokens array */
-        free(tokens);
-
+	if (!token)
+		return (-1);
+/* walk through other tokens */
+	while (token != NULL)
+	{
+/* token is equal to the count of tokens */
+		tokens[count++] = token;
+/* if buffer size is reached, function has failed */
+		if (count == bufsize)
+			return (-1);
+/* token is equal to the last token which is NULL */
+		token = strtok(NULL, delim);
+	}
+/* return number of tokens */
+/*	printf("%s\n", tokens[count - 1]);*/
+	print_string(tokens[count - 1]);
+	print_string("\n");
+	tokens[count] = NULL;
+	return (count);
 }
